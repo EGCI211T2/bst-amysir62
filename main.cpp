9895d5dd74691f485580b0b003cbaebad3097c85
amysir62 <amy.sir@student.mahidol.edu>
2026-03-30 09:27:29 +0000

    UwU

--- main.cpp
+++ main.cpp
@@ -2,14 +2,32 @@
 #include <iomanip>
 using namespace std;
 #include "BST.h"
-int main(int argc, char **argv) {
-  unsigned int i; // counter to loop from 1-10
-  BST b;
-  for (i = 1; i < argc; ++i) {
-       b.insert_node (atoi(argv[i]));
-  } // end for
-   b.print();//in order function
 
-  //b.searchtree(11);
- 
-} // end main
+
+int main(int argc, char **argv) {
+  BST b;
+
+  for (int i = 1; i < argc; ++i) {
+    b.insert_node(atoi(argv[i]));
+  }
+
+  cout << "inOrder" << endl <<endl;
+  b.print(0);
+  cout << endl;
+
+  cout << "preOrder" << endl<<endl;
+  b.print(1);
+  cout << endl;
+
+  cout << "postOrder" << endl<<endl;
+  b.print(2);
+  cout << endl;
+
+
+  cout << "Tree" << endl<<endl;
+  cout << endl;
+
+  b.print(4);   // default → printTree
+
+  return 0;
+}
